fd7f7d4f546d2f5825a58160ff5755d7929fb91d
Adam Szałański <adsn@softwaremind.com>
2024-09-23 20:28:00 +0200

    refactor: drobne zmiany w test containerach pod development

--- src/main/java/org/aszjch/demoapp/infrastructure/publisher/KafkaArticlePublisherImpl.java
+++ src/main/java/org/aszjch/demoapp/infrastructure/publisher/KafkaArticlePublisherImpl.java
@@ -1,10 +1,12 @@
 package org.aszjch.demoapp.infrastructure.publisher;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.aszjch.demoapp.domain.port.ArticlePublisher;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 class KafkaArticlePublisherImpl implements ArticlePublisher {
@@ -13,6 +15,7 @@ class KafkaArticlePublisherImpl implements ArticlePublisher {
 
     @Override
     public void publish(String message) {
+        log.debug("Publishing message: [{}]", message);
         kafkaTemplate.send("articles", message);
     }
 }
