691243c57a43066c4c5545064b4a415428edd44b
Adam Szałański <adamus8895@gmail.com>
2024-09-30 22:21:16 +0200

    refactor(KafkaTopicConfig): zmiana nazwy klasy na KafkaConfig

--- src/main/java/org/aszjch/demoapp/infrastructure/config/KafkaConfig.java
+++ src/main/java/org/aszjch/demoapp/infrastructure/config/KafkaConfig.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
-class KafkaTopicsConfig {
+class KafkaConfig {
 
     @Bean
     public NewTopic articlesTopic() {
-        return TopicBuilder.name("articles")
+        return TopicBuilder.name("articles-created")
                 .partitions(1)
                 .replicas(1)
                 .build();
