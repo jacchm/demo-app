8089dfe893046921599b57001e2647d0df360fcf
Adam Szałański <adamus8895@gmail.com>
2024-10-03 20:15:32 +0200

    test(MinIOArticleFileStorageServiceIT): skrócenie testu publikacji pliku na minio

--- src/test/java/org/aszjch/demoapp/domain/articlefile/port/MinIOArticleFileStorageServiceIT.java
+++ src/test/java/org/aszjch/demoapp/domain/articlefile/port/MinIOArticleFileStorageServiceIT.java
@@ -1,15 +1,14 @@
 package org.aszjch.demoapp.domain.articlefile.port;
 
 
-import io.minio.ListObjectsArgs;
 import io.minio.MinioClient;
-import io.minio.Result;
-import io.minio.messages.Item;
+import io.minio.StatObjectArgs;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.aszjch.demoapp.TestcontainersConfiguration;
 import org.aszjch.demoapp.domain.articlefile.ArticleFile;
 import org.aszjch.demoapp.infrastructure.config.MinIOConnetionDetails;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +16,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
@@ -37,27 +36,30 @@ class MinIOArticleFileStorageServiceIT {
     @Test
     void publishes_article_file_to_minio(@TempDir final File tempDir) throws Exception {
         final String fileName = "test-file.txt";
+        final File file = prepareTestFile(tempDir, fileName);
+        final ArticleFile articleFile = prepareArticleFile(file, fileName);
+
+        minIOArticleFileStorageService.store(articleFile);
+
+        assertDoesNotThrow(() -> minioClient.statObject(StatObjectArgs.builder()
+                                                                .bucket(minIOConnetionDetails.bucket())
+                                                                .object(fileName)
+                                                                .build()),
+                           "File doesn't exist in MinIO bucket");
+    }
+
+    private @NotNull ArticleFile prepareArticleFile(final File file, final String fileName) {
+        final ArticleFile articleFile = new ArticleFile();
+        articleFile.setFile(file);
+        articleFile.setFilename(fileName);
+        return articleFile;
+    }
+
+    private @NotNull File prepareTestFile(final File tempDir, final String fileName) throws IOException {
         final File file = new File(tempDir + "/" + fileName);
         file.createNewFile();
         final String fileContent = "Hello from Testcontainers";
         FileUtils.write(file, fileContent, StandardCharsets.UTF_8);
-        final ArticleFile articleFile = new ArticleFile();
-        articleFile.setFile(file);
-        articleFile.setFilename(fileName);
-
-        minIOArticleFileStorageService.store(articleFile);
-
-
-        final Iterable<Result<Item>> results = minioClient.listObjects(ListObjectsArgs.builder()
-                                                                               .bucket(minIOConnetionDetails.bucket())
-                                                                               .build());
-        final ArrayList<Result<Item>> resultList = new ArrayList<>();
-        results.forEach(resultList::add);
-        assertEquals(1, resultList.size());
-        final Item firstResult = resultList.getFirst()
-                .get();
-        assertEquals(fileName, firstResult.objectName());
-        log.info("Filename read from minio: {}", firstResult.objectName());
-
+        return file;
     }
 }
