796fe4fbf4a52a2b5fa2c7e104ba4685f7bba278
Adam Szałański <adamus8895@gmail.com>
2024-10-06 21:14:22 +0200

    refactor: data w ArticleDto

--- src/main/java/org/aszjch/demoapp/aplication/ArticleDto.java
+++ src/main/java/org/aszjch/demoapp/aplication/ArticleDto.java
@@ -1,11 +1,9 @@
 package org.aszjch.demoapp.aplication;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 class ArticleDto {
 
     @JsonIgnore
