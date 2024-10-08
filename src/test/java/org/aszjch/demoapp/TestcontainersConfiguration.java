2d7fa92250e495815d034ee32b2f466a99e6e775
Adam Szałański <adamus8895@gmail.com>
2024-10-08 20:42:18 +0200

    feat: kafka-UI jako przykład generic container

--- src/test/java/org/aszjch/demoapp/TestcontainersConfiguration.java
+++ src/test/java/org/aszjch/demoapp/TestcontainersConfiguration.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.MinIOContainer;
+import org.testcontainers.containers.Network;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.PostgreSQLContainerProvider;
 import org.testcontainers.utility.DockerImageName;
@@ -15,9 +17,19 @@ import org.testcontainers.utility.DockerImageName;
 public class TestcontainersConfiguration {
 
     @Bean
+    Network kafkaNetwork() {
+        return Network.newNetwork();
+    }
+
+    @Bean
     @ServiceConnection
-    KafkaContainer kafkaContainer() {
-        return new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
+    KafkaContainer kafkaContainer(final Network kafkaNetwork) {
+        final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"))
+                .withNetwork(kafkaNetwork)
+                .withEmbeddedZookeeper()
+                .withNetworkAliases("kafka");
+        kafka.start();
+        return kafka;
     }
 
     @Bean
@@ -33,6 +45,16 @@ public class TestcontainersConfiguration {
     @ServiceConnection
     MinIOContainer minioContainer() {
         return new MinIOContainer(DockerImageName.parse("minio/minio:latest"));
+    }
+
+    @Bean
+    GenericContainer<?> kafkaUiContainer(final KafkaContainer kafkaContainer, final Network kafkaNetwork) {
+        return new GenericContainer<>(DockerImageName.parse("provectuslabs/kafka-ui:latest"))
+                .withNetwork(kafkaNetwork)
+                .withExposedPorts(8080)
+                .withEnv("KAFKA_CLUSTERS_0_NAME", "local")
+                .withEnv("KAFKA_CLUSTERS_0_BOOTSTRAP_SERVERS", "kafka:9092")
+                .dependsOn(kafkaContainer);
     }
 
 }
